2f70b0adc584e5647e967682460ba52170ec3dea
Alexkawai <leha.iuganov@yandex.ru>
2021-07-11 16:56:05 +0300

    done all!!!

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -2,10 +2,16 @@ package web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import web.dao.Dao;
 import web.model.User;
+
+import java.util.List;
 
 @Controller
 public class UserController {
@@ -15,12 +21,40 @@ public class UserController {
     public void setDao(Dao dao) {
         this.dao = dao;
     }
+
     @GetMapping(value = "/")
     public String printUser(ModelMap model) {
-        User user = dao.getById(1L);
-
-        //User user = new User("Vasya","Petechkin","vasya.petechkin@yandex.ru");
-        model.addAttribute("user", user);
+        List<User> users= dao.allUsers();
+        model.addAttribute("users", users);
         return "index";
+    }
+
+    @GetMapping(value = "/edit/{id}")
+    public String editUser(@PathVariable("id") long id, ModelMap model) {
+        User user = dao.getById(id);
+        model.addAttribute("user", user);
+        return "edit";
+    }
+    @PostMapping(value = "/edit")
+    public String edit( User user) {
+        dao.edit(user);
+        return "redirect:/";
+    }
+
+    @GetMapping(value = "/delete/{id}")
+    public String deleteUser(@PathVariable("id") long id, ModelMap model) {
+        dao.delete(id);
+        return "redirect:/";
+    }
+
+    @GetMapping(value = "/add")
+    public String addUser(Model model){
+        model.addAttribute("user",new User());
+        return "add";
+    }
+    @PostMapping(value = "/add")
+    public String create( User user) {
+        dao.save(user);
+        return "redirect:/";
     }
 }
--- src/main/java/web/dao/Dao.java
+++ src/main/java/web/dao/Dao.java
@@ -27,50 +27,47 @@ public class Dao {
     }
 
 
-
-
-
-/*
-    public List<User> allFilms() {
-        Session session = sessionFactory.getCurrentSession();
-        return session.createQuery("from User",User.class).list();
-    }
-
-
-
-
-
-    public void delete(User user) {
-        Session session = sessionFactory.getCurrentSession();
-        session.delete(user);
-    }
-
-
-    public void edit(User user) {
-        Session session = sessionFactory.getCurrentSession();
-        session.update(user);
-    }
-
-*/
     public void save(User user) {
         EntityManager em =  lcemfb.getObject().createEntityManager();
-
         em.getTransaction().begin();
-        em.persist(user);
+        em.merge(user);
         em.getTransaction().commit();
+        em.close();
     }
     public User getById(Long id) {
 
         EntityManager em = lcemfb.getObject().createEntityManager();
         User user = em.find(User.class, id);
         em.detach(user);
+        em.close();
         return user;
     }
-    public void delete(User user) {
+    public void delete(Long id) {
         EntityManager em = lcemfb.getObject().createEntityManager();
         em.getTransaction().begin();
+        User user = em.find(User.class,id);
         em.remove(user);
         em.getTransaction().commit();
+        em.close();
+    }
+    public List<User> allUsers() {
+        EntityManager em = lcemfb.getObject().createEntityManager();
+        List<User> users = em.createQuery("from User",User.class).getResultList();
+        em.close();
+        return users;
+    }
+    public void edit(User user) {
+        EntityManager em = lcemfb.getObject().createEntityManager();
+        em.getTransaction().begin();
+        User user2=em.find(User.class, user.getId());
+        user2.setId(user.getId());
+        user2.setName(user.getName());
+        user2.setLastName(user.getLastName());
+        user2.setEmail(user.getEmail());
+
+        em.merge(user2);
+        em.getTransaction().commit();
+        em.close();
     }
 
 
